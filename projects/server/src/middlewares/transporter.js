21ef44960f9bae34dc303597ba0407499cb614e7
rifkyrizkita <rifky.rizkita.g@gmail.com>
2023-07-28 13:53:12 +0700

    v1.1
    test

--- projects/server/src/middlewares/transporter.js
+++ projects/server/src/middlewares/transporter.js
@@ -3,8 +3,8 @@ const nodemailer = require("nodemailer")
 const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-        user:"rifky.rizkita@gmail.com",
-        pass:"gxmzebjfuzigkufm"
+        user:process.env.EMAIL_TRANSPORTER,
+        pass:process.env.PASS_TRANSPORTER
     },
     tls : {
         rejectUnauthorized : false
